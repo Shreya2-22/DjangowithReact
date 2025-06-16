0e048821c0c755e01157ba98fda36fe478523ee6
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-16 12:53:02 +0545

    add backend url

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,58 +1,77 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../api";
-import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/FormStyles.css";
 import LoadingIndicator from "../components/LoadingIndicator";
 
 function Form({ route, method }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        setLoading(true)
-        e.preventDefault();
-        try {
-            const res = await api.post(route, { username, password });
-            if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/");
-            } else {
-                navigate("/login");
-            }
-        } catch (error) {
-            alert(error);
-        } finally {
-            setLoading(false)
-        }
-    };
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="form-container">
-            <h1>{method === "login" ? "Login" : "Register"}</h1>
-            <input
-                className="form-input"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-            />
-            <input
-                className="form-input"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
-            {loading && <LoadingIndicator />}
-            <button className="form-button" type="submit">
-                {method === "login" ? "Login" : "Register"}
-            </button>
-        </form>
-    );
+  return (
+    <div className="form-wrapper">
+      <form onSubmit={handleSubmit} className="form-container">
+        <h1>{method === "login" ? "Login" : "Register"}</h1>
+
+        <input
+          className="form-input"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          className="form-input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+
+        {loading && <LoadingIndicator />}
+
+        <button className="form-button" type="submit">
+          {method === "login" ? "Login" : "Register"}
+        </button>
+      </form>
+
+      <div className="form-footer">
+        {method === "login" ? (
+          <p>
+            New user? <Link to="/register">Register here</Link>
+          </p>
+        ) : (
+          <p>
+            Already have an account? <Link to="/login">Login here</Link>
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Form;
